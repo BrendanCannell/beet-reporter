6b700a470895e2338956dc043ed23909d56c6e28
Brendan Cannell <brendan@cannells.com>
2019-01-23 21:53:14 -0500

    ESC aborts edits instead of commiting them

--- react-ui/src/Note/index.js
+++ react-ui/src/Note/index.js
@@ -16,7 +16,7 @@ export default class Note extends React.Component {
     this.state = {
       body: this.props.body,
       active: false,
-      justActivated: false
+      justFlippedActive: false
     }
 
     this.ref = React.createRef()
@@ -38,11 +38,7 @@ export default class Note extends React.Component {
     this.submitEdit = this.submitEdit.bind(this)
   }
 
-  submitEdit(e) {
-    this.setState({active: false})
-
-    if (this.props.body === this.state.body) return
-
+  submitEdit() {
     let notes = this.props.app.state.notes,
         oldNote = notes[this.props._id],
         newNote = {...oldNote, body: this.state.body}
@@ -57,10 +53,18 @@ export default class Note extends React.Component {
   }
 
   componentDidUpdate() {
-    if (this.state.active && this.state.justActivated) {
-      this.ref.current.focus()
-      this.ref.current.setSelectionRange(this.state.body.length, this.state.body.length)
-      this.setState({justActivated: false})
+    if (this.state.justFlippedActive) {
+      if (this.state.active) {
+        this.ref.current.focus()
+        this.ref.current.setSelectionRange(this.state.body.length, this.state.body.length)
+      } else {
+        //this.ref.current.blur()
+        if (this.props.body !== this.state.body) {
+          this.submitEdit()
+        }
+      }
+
+      this.setState({justFlippedActive: false})
     }
   }
 
@@ -72,13 +76,16 @@ export default class Note extends React.Component {
           type="text"
           value={this.state.body}
           onChange={e => this.setState({body: e.target.value})}
-          onBlur={e => this.submitEdit(e)}
-          onKeyUp={e => [ENTER, ESC].includes(e.keyCode) && e.target.blur()}
+          onBlur={() => this.setState({active: false, justFlippedActive: true})}
+          onKeyUp={e => 
+              e.keyCode === ENTER ? this.setState({active: false, justFlippedActive: true})
+            : e.keyCode === ESC   ? this.setState({active: false, justFlippedActive: true, body: this.props.body})
+            : null}
           ref={this.ref}
         />
       : <span
           onClick={e => {
-            this.setState({active: true, justActivated: true})
+            this.setState({active: true, justFlippedActive: true})
             e.target.focus()
           }}
         >
